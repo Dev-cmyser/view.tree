0160c3a4919bbeaeb8b17b171cf65d892a93b1f9
novgorodcev.k <novgorodcev.k@rwb.ru>
2025-09-06 11:56:25 +0300

    add logs

--- vscode/extension.ts
+++ vscode/extension.ts
@@ -1,9 +1,14 @@
 import * as path from 'path'
-import { workspace, ExtensionContext } from 'vscode'
+import { workspace, ExtensionContext, window } from 'vscode'
 import { LanguageClient, TransportKind, ServerOptions, LanguageClientOptions } from 'vscode-languageclient/node'
 
+let client: LanguageClient | undefined
+
 export function activate(context: ExtensionContext) {
-	const serverModule = context.asAbsolutePath(path.join('server', 'out', 'index.js'))
+	const serverModule = context.asAbsolutePath(path.join('out', 'server', 'index.js'))
+	const traceOutput = window.createOutputChannel('view.tree LSP', { log: true })
+	traceOutput.appendLine('[view.tree] Activating client…')
+	traceOutput.show(true)
 	const serverOptions: ServerOptions = {
 		run: { module: serverModule, transport: TransportKind.ipc },
 		debug: { module: serverModule, transport: TransportKind.ipc, options: { execArgv: ['--inspect=6009'] } },
@@ -11,7 +16,14 @@ export function activate(context: ExtensionContext) {
 	const clientOptions: LanguageClientOptions = {
 		documentSelector: [{ scheme: 'file', language: 'view.tree' }],
 		synchronize: { fileEvents: workspace.createFileSystemWatcher('**/*.view.tree') },
+		traceOutputChannel: traceOutput,
 	}
-	const client = new LanguageClient('viewtree-lsp', 'view.tree LSP', serverOptions, clientOptions)
-	context.subscriptions.push(client.start())
+	client = new LanguageClient('viewtree-lsp', 'view.tree LSP', serverOptions, clientOptions)
+	context.subscriptions.push(client)
+	client.start()
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) return undefined
+	return client.stop()
 }
